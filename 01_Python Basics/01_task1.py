8920525d5f241b39c319e32f911971c1fbc53119
Z. Zaremska (Epam) <zuzanna_zaremska@epam.com>
2024-09-17 09:52:34 +0200

    Added soring of the list without using sort() function.

--- 01_Python Basics/01_task1.py
+++ 01_Python Basics/01_task1.py
@@ -4,16 +4,22 @@ from statistics import mean
 
 # Create list of 100 random numbers from 0 to 1000
 random_numbers = random.sample(range(0, 1000), 100)
-print(random_numbers)
 
 # Sort list from min to max (without using sort()).
-sorted_random_numbers = []
+for idx_1 in range(0, len(random_numbers)):
+    # Compare only numbers on the right side from current (idx_1) index.
+    for idx_2 in range(idx_1 + 1, len(random_numbers)):
+        # If number from the index equal idx_1 is bigger or equal to number
+        # from the index equal idx_2 swap them by places.
+        # Example: if at index 2 there is number 3, and at index 5 there is number 2 - swap.
+        if random_numbers[idx_1] >= random_numbers[idx_2]:
+            random_numbers[idx_1], random_numbers[idx_2] = random_numbers[idx_2], random_numbers[idx_1]
 
 # Calculate average for even and odd numbers.
 # Odd number is when the result of dividing by modulo operator is not equal zero.
-odd_numbers = [num for num in sorted_random_numbers if num % 2 != 0]
+odd_numbers = [num for num in random_numbers if num % 2 != 0]
 avg_odd_numbers = mean(odd_numbers)
-even_numbers = [num for num in sorted_random_numbers if num % 2 == 0]
+even_numbers = [num for num in random_numbers if num % 2 == 0]
 avg_even_numbers = mean(even_numbers)
 
 # Print both average result in console.
