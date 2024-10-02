c7a22954f7ca67bcb2e51c08eb1d24eaef45b884
zuza-zaremska-epm <zuzanna_zaremska@epam.com>
2024-10-02 18:55:20 +0200

    Restructured code.

--- 06_Module FIle/feeds.py
+++ 06_Module FIle/feeds.py
@@ -1,9 +1,3 @@
-# Expand previous Homework 5 with additional class, which allow to provide records by text file:
-# 1.Define your input format (one or many records)
-# 2.Default folder or user provided file path
-# 3.Remove file if it was successfully processed
-# 4.Apply case normalization functionality form Homework 3/4
-
 import pendulum
 import os
 
@@ -11,8 +5,7 @@ import os
 class Feed:
     file_path = 'Feed.txt'
 
-    def __init__(self, text):
-        self.text = text
+    def __init__(self):
         self.insert_date = pendulum.now()
         self.feed = ''
 
@@ -30,28 +23,31 @@ class Feed:
 
 
 class News(Feed):
-    def __init__(self, text, city):
-        super().__init__(text)
-        self.city = city
+    def __init__(self):
+        super().__init__()
+        self.city = input('Provide city name: ').lower().title()
+        self.text = input('Provide news text: ')
         self.feed = f'\n--- NEWS ---\n{self.text}\n{self.city}, {self.insert_date.format("YYYY-MM-DD HH:mm")}'
         print(self.feed)
 
 
 class PrivateAd(Feed):
-    def __init__(self, text: str, exp_date: str):
-        super().__init__(text)
-        self.exp_date = pendulum.parse(exp_date, strict=False).date()
+    def __init__(self):
+        super().__init__()
+        self.text = input('Provide advertisement text: ')
+        self.exp_date = pendulum.parse(input('Provide expiration date (YYYY-MM-DD): '), strict=False).date()
         self.days_left = (self.exp_date - self.insert_date.date()).days
         self.feed = f'\n--- PRIVATE AD ---\n{self.text}\nActual until: {self.exp_date.format("YYYY-MM-DD")} ({self.days_left} days left)'
         print(self.feed)
 
 
 class Journal(Feed):
-    def __init__(self, text, name, mood_name):
-        super().__init__(text)
-        self.name = name.title()
-        self.mood = mood_name.lower()
-        self.feed = f'\n--- JOURNAL ---\n{self.text}\nI feel {mood_name} today.\n{self.name}, {self.insert_date.format("YYYY-MM-DD HH:mm")}'
+    def __init__(self):
+        super().__init__()
+        self.text = input('Provide journal text: ')
+        self.name = input('Provide your name ').lower().title()
+        self.mood = input("What's your mood today: ").lower()
+        self.feed = f'\n--- JOURNAL ---\n{self.text}\nI feel {self.mood} today.\n{self.name}, {self.insert_date.format("YYYY-MM-DD HH:mm")}'
         print(self.feed)
 
 
@@ -103,27 +99,16 @@ def create_feed_by_category(category: str):
     Creates feed by the given category.
     :param category: name of the feed category/type
     """
+    category = category.lower()
+
     if category in ['news', 'new']:
-        user_city = input('Provide city name: ').lower().title()
-        news_text = input('Provide news text: ')
-
-        news = News(news_text, user_city)
-        news.save_feed()
-
+        feed = News()
     elif category in ['private ad', 'private', 'ad', 'priv']:
-        ad_text = input('Provide advertisement text: ')
-        expiration_date = input('Provide expiration date (YYYY-MM-DD): ')
-
-        ad = PrivateAd(ad_text, expiration_date)
-        ad.save_feed()
-
+        feed = PrivateAd()
     else:
-        mood = input("What's your mood today: ").lower()
-        journal_text = input('Provide journal text: ')
-        user_name = input('Provide your name ').lower().title()
+        feed = Journal()
 
-        journal = Journal(journal_text, user_name, mood)
-        journal.save_feed()
+    feed.save_feed()
 
 
 # Create new feed file if not exists.
@@ -131,7 +116,7 @@ Feed.create_feed_file()
 
 while True:
     # Take an information from user.
-    user_category = input('What category you want to add?\n"News" | "Private ad" | "Journal": ').lower()
+    user_category = input('What category you want to add?\n"News" | "Private ad" | "Journal": ')
     create_feed_by_category(user_category)
 
     next_insert = input('Do you want to insert another (y/n)? ').lower()
