421801894eaac55a64e3def7820a12f129e10217
zuza-zaremska-epm <zuzanna_zaremska@epam.com>
2024-10-06 17:33:24 +0200

    Adjusted process to the changes made in Input class.

--- 06_Module FIle/main.py
+++ 06_Module FIle/main.py
@@ -6,64 +6,59 @@
 from feeds import Input, Feed, News, PrivateAd, Journal
 
 
-def create_feed_by_category(category: str, input_collection: dict):
+def create_feed_by_category(input_details: dict):
     """
-    Creates feed by the given category.
-    :param category: name of the feed category
-    :param input_collection: data provided for the feed
+    Creates feed by the given input details.
+    :param input_details: data provided for the feed
     """
-    category = category.lower()
+    category = input_details.get('category').lower()
 
-    if category in ['news', 'new']:
-        text = input_collection.get('text')
-        city = input_collection.get('city')
-        feed = News(text, city)
-    elif category in ['private ad', 'private', 'ad', 'priv']:
-        text = input_collection.get('text')
-        exp_date = input_collection.get('exp_date')
-        feed = PrivateAd(text, exp_date)
+    if not category:
+        print('Category not provided - impossible to create.')
     else:
-        text = input_collection.get('text')
-        name = input_collection.get('name')
-        mood = input_collection.get('mood')
-        feed = Journal(text, name, mood)
+        if category in ['news', 'new']:
+            text = input_details.get('text')
+            city = input_details.get('city')
+            feed = News(text, city)
+        elif category in ['private ad', 'private', 'ad', 'priv']:
+            text = input_details.get('text')
+            exp_date = input_details.get('exp_date')
+            feed = PrivateAd(text, exp_date)
+        else:
+            text = input_details.get('text')
+            name = input_details.get('name')
+            mood = input_details.get('mood')
+            feed = Journal(text, name, mood)
 
-    feed.save_feed()
-
-
-# Get information about data ingestion type.
-ingestion = input('Do you want to enter data manually/by file? (m/f) ').lower()
-if ingestion in ['f', 'by file', 'file']:
-    custom_path = input('Do you want to provide path to the file? (y/n) ')
-    if custom_path.lower() in ['y', 'yes']:
-        custom_path = input('Enter path to the txt file with the input: ').lower()
-        file_input = Input(path=custom_path)
-        file_input.read_input_parameters()
-    else:
-        file_input = Input()
-        file_input.get_input_from_all_files()
+        feed.save_feed()
 
 
 # Create new feed file if not exists.
 Feed.create_feed_file()
-feed_counter = 0
-# Populate feed file with data.
-while True:
-    if ingestion in ['f', 'by file', 'file']:
-        feed_counter += 1
-        input_data = file_input.input[feed_counter]
-        feed_category = input_data.get('category')
-        create_feed_by_category(feed_category, input_data)
+get_feeds = True
 
-        if feed_counter == len(file_input.input):
-            break
+while get_feeds:
+    # Get information about data ingestion type.
+    ingestion = input('Do you want to enter data manually/by file? (m/f) ').lower()
+    if ingestion in ['f', 'by file', 'file', 'files']:
+        file_input = Input()
+        custom_path = input('Do you want to provide path to the file? (y/n) ')
+        if custom_path.lower() in ['y', 'yes']:
+            file_input.get_path_from_user()
+        else:
+            file_input.get_paths_from_default_directory()
 
+        file_input.get_input_from_files()
+        for filename, feeds_details in file_input.input_data.items():
+            for feed_params in feeds_details:
+                create_feed_by_category(feed_params)
     else:
         user_category = input('What category you want to add?\n"News" | "Private ad" | "Journal": ')
-        create_feed_by_category(user_category, {})
+        feed_params = {'category': user_category}
+        create_feed_by_category(feed_params)
 
-        next_insert = input('Do you want to insert another (y/n)? ')
-        if next_insert.lower() not in ['y', 'yes']:
-            break
+    next_insert = input('Do you want to insert another (y/n)? ')
+    if next_insert.lower() not in ['y', 'yes']:
+        get_feeds = False
 
 print('\nFile has been saved.')
