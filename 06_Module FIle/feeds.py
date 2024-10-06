e3b7096871be121fe029c84c3e800a0df4c10de5
zuza-zaremska-epm <zuzanna_zaremska@epam.com>
2024-10-06 18:13:13 +0200

    Added function for text normalization. Added abstract method for creating feed text.

--- 06_Module FIle/feeds.py
+++ 06_Module FIle/feeds.py
@@ -10,7 +10,7 @@ class Feed(ABC):
 
     def __init__(self, text):
         self.text = text
-        self.feed = ''
+        self.feed = None
         self.insert_date = pendulum.now()
 
     @classmethod
@@ -20,15 +20,38 @@ class Feed(ABC):
             with open(cls.file_path, 'w', encoding='utf-8') as file:
                 file.write('NEWS FEED\n')
 
-    def save_feed(self):
-        """Saves created feed in the txt file."""
-        with open(Feed.file_path, 'a', encoding='utf-8') as file:
-            file.write(self.feed + '\n')
-
     @abstractmethod
     def get_data_from_user(self):
         """Overwrite attributes with data from the user."""
         pass
+
+    @abstractmethod
+    def create_feed(self):
+        """Creates feed in the form characteristic for specific feed type."""
+        pass
+
+    def save_feed(self):
+        """Saves created feed in the txt file."""
+        self.create_feed()
+        with open(Feed.file_path, 'a', encoding='utf-8') as file:
+            file.write(self.feed + '\n')
+
+    def normalize_text(self):
+        """
+        Normalizes text attribute to the form where each sentence starts with
+        the capital letter.
+        """
+        lines = self.text.lower().split('.')
+
+        for line_index, line in enumerate(lines):
+            for char in line:
+                if char not in whitespace:
+                    line = line.replace(char, char.upper(), 1)
+                    break
+
+            lines[line_index] = line
+
+        self.text = '.'.join(lines)
 
 
 class News(Feed):
@@ -37,30 +60,35 @@ class News(Feed):
         self.city = city
         if not text:
             self.get_data_from_user()
-
-        self.feed = f'\n--- NEWS ---\n{self.text}\n{self.city}, {self.insert_date.format("YYYY-MM-DD HH:mm")}'
-        print(self.feed)
+        self.normalize_text()
 
     def get_data_from_user(self):
         self.city = input('Provide city name: ').title()
         self.text = input('Provide news text: ')
 
+    def create_feed(self):
+        self.feed = f'\n--- NEWS ---\n{self.text}\n{self.city}, {self.insert_date.format("YYYY-MM-DD HH:mm")}'
+        print(self.feed)
+
 
 class PrivateAd(Feed):
     def __init__(self, text: str, exp_date: str):
         super().__init__(text)
-        self.exp_date = pendulum.parse(exp_date, strict=False).date()
+        self.exp_date = exp_date
         if not text:
             self.get_data_from_user()
+        self.normalize_text()
 
+        self.exp_date = pendulum.parse(self.exp_date, strict=False).date()
         self.days_left = (self.exp_date - self.insert_date.date()).days
-        self.feed = f'\n--- PRIVATE AD ---\n{self.text}\nActual until: {self.exp_date.format("YYYY-MM-DD")} ({self.days_left} days left)'
-        print(self.feed)
 
     def get_data_from_user(self):
         self.text = input('Provide advertisement text: ')
-        expiration_date = input('Provide expiration date (YYYY-MM-DD): ')
-        self.exp_date = pendulum.parse(expiration_date, strict=False).date()
+        self.exp_date = input('Provide expiration date (YYYY-MM-DD): ')
+
+    def create_feed(self):
+        self.feed = f'\n--- PRIVATE AD ---\n{self.text}\nActual until: {self.exp_date.format("YYYY-MM-DD")} ({self.days_left} days left)'
+        print(self.feed)
 
 
 class Journal(Feed):
@@ -70,14 +98,16 @@ class Journal(Feed):
         self.mood = mood
         if not text:
             self.get_data_from_user()
-
-        self.feed = f'\n--- JOURNAL ---\n{self.text}\nI feel {self.mood} today.\n{self.name}, {self.insert_date.format("YYYY-MM-DD HH:mm")}'
-        print(self.feed)
+        self.normalize_text()
 
     def get_data_from_user(self):
         self.text = input('Provide journal text: ')
         self.name = input('Provide your name: ').title()
         self.mood = input("What's your mood today: ").lower()
+
+    def create_feed(self):
+        self.feed = f'\n--- JOURNAL ---\n{self.text}\nI feel {self.mood} today.\n{self.name}, {self.insert_date.format("YYYY-MM-DD HH:mm")}'
+        print(self.feed)
 
 
 class Input:
